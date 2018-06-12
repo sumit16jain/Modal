bfcdebd652c3c5cb9ce9793fc25ea7b47db29fe5
sumit <sumit2192jain@gmail.com>
2018-06-12 16:30:44 +0530

    commit

---
+++ Modal.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+//----- OPEN
+$('[data-popup-open]').on('click', function(e) {
+var targeted_popup_class = $(this).attr('data-popup-open');
+$('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+e.preventDefault();
+});
+//----- CLOSE
+$('[data-popup-close]').on('click', function(e) {
+var targeted_popup_class = $(this).attr('data-popup-close');
+$('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+e.preventDefault();
+});
+});
